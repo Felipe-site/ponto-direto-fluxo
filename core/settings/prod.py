8d4abfdf8deb3d5c13d841e1efcf79cf36994153
Murilo Barboza <murilobarboza04@gmail.com>
2025-06-20 23:07:03 -0300

    Ajustando proxy no deploy

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -15,3 +15,8 @@ DATABASES = {
 }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
