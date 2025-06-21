27d17355861962e96de1de9d42ebeffc41bded1a
Murilo Barboza <murilobarboza04@gmail.com>
2025-06-20 22:54:10 -0300

    Prod.py corrigido

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,22 +1,7 @@
-import os
-print("--- INICIANDO O CARREGAMENTO DE PROD.PY (MODO DEBUG) ---")
+from .base import *
 
-try:
-    # Tentamos importar tudo do base.py. É aqui que o erro real está acontecendo.
-    from .base import *
-    print("--- SUCESSO: base.py FOI IMPORTADO SEM ERROS ---")
-
-except Exception as e:
-    # Se qualquer erro acontecer durante a importação do base.py, vamos capturá-lo.
-    print("!!!!!!!! ERRO CRÍTICO AO IMPORTAR BASE.PY !!!!!!!!")
-    import traceback
-    traceback.print_exc() # Imprime o traceback completo do erro real.
-    raise e
-
-# Se o base.py foi importado com sucesso, o código continua...
 DEBUG = False
 
-# Usamos config() aqui também para ler as variáveis de ambiente
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
@@ -30,7 +15,3 @@ DATABASES = {
 }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-print(f"--- DEBUG PROD: DEBUG={DEBUG} ---")
-print(f"--- DEBUG PROD: ALLOWED_HOSTS={ALLOWED_HOSTS} ---")
-print("--- FIM DO CARREGAMENTO DE PROD.PY ---")
