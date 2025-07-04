7b34b8be530eda70cf9eb0835ee4e6b7e3eea7f5
Murilo Barboza <murilobarboza04@gmail.com>
2025-07-04 18:57:36 -0300

    Habilitando extensão do PostgreSQL

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
+echo "Habilitando extensões do PostgreSQL..." 
 python manage.py enable_postgres_extensions
 
 python manage.py createsuperuser --noinput || true
