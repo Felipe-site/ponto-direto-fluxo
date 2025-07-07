183d28c6a8b6bca8ae38ab2ce297e2e43a2cc5c5
Murilo Barboza <murilobarboza04@gmail.com>
2025-07-06 23:22:05 -0300

    Corrigindo img do boleto

--- src/pages/ProdutoDetalhe.tsx
+++ src/pages/ProdutoDetalhe.tsx
@@ -161,12 +161,12 @@ const ProdutoDetalhe = () => {
                       <span>Cartão de débito</span>
                     </div>
                     <div className="flex items-center justify-center gap-2">
-                      <img src="/pix.png" alt="Pix" className="h-4 w-4 mr-1" />
-                      <span>Pagamento via Pix</span>
+                      <img src="/boleto.png" alt="Boleto" className="h-4 w-8 mr-1" />
+                      <span>Boleto</span>
                     </div>
                     <div className="flex items-center justify-center gap-2">
-                      <img src="/boleto.png" alt="Boleto" className="h-4 w-4 mr-1" />
-                      <span>Boleto</span>
+                      <img src="/pix.png" alt="Pix" className="h-4 w-4 mr-1" />
+                      <span>Pagamento via Pix</span>
                     </div>
                   </div>
                   
